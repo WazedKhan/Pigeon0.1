823fed561d9284ec86c993d4f0b1c8e2294c02cf
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2021-11-13 11:30:11 +0600

    returned user data

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ProfilesController extends Controller
 {
     public function index()
     {
-        return view('profiles.index');
+        $user = User::find(1);
+        //dd($data);
+        return view('profiles.index', [
+            'user'=>$user,
+        ]);
     }
 }
