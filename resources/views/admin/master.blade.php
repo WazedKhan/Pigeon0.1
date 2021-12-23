632738c02c7dc834b8fe350528fb74ebccc58417
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2021-12-23 12:02:42 +0600

    fontawsome

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -10,8 +10,7 @@
     <link rel="stylesheet" href="/css/bootstrap.min.css"
         crossorigin="anonymous">
     <link href="/css/admin.css" rel="stylesheet">
-    <link href="/css/fontawesome.min.css" rel="stylesheet">
-    <link href="/css/fontawesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
  
 
 </head>
