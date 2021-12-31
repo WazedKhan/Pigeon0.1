c39d913362404d55525b9f86ed3e2def3cb83659
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2021-12-31 22:59:26 +0600

    added fontawsome css

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="/css/bootstrap.min.css">
 
     <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
     <title>Pigeon</title>
 </head>
