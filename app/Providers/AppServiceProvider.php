037ba5ee82f9830acfeb389cd02d5f7a6895148e
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-01-11 23:06:48 +0600

    view composer to sent data selected view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // View::share('public_data', User::latest()->get());
+        View::composer(['layout.sidebar'], function($view){
+            $view->with('user_info',User::latest()->get());
+        });
     }
 }
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -2,10 +2,9 @@
     <h3>Our Sidebar</h3>
     <p class='text-muted'>You can put any information here you'd like.
       <ul class="list-group">
-        <li class="list-group-item list-group-item-light">Latest Posts</li>
-        <li class="list-group-item list-group-item-light">Announcements</li>
-        <li class="list-group-item list-group-item-light">Calendars</li>
-        <li class="list-group-item list-group-item-light">etc</li>
+          @foreach ($user_info as $item)
+          <li class="list-group-item list-group-item-light">{{ $item->name }}</li>
+          @endforeach
       </ul>
     </p>
     <form class="d-flex">
