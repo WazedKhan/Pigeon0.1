947ef6e243956f8d98c45d6cc0ff2c97a9b374ac
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-01-24 22:58:24 +0600

    changed ddddd

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,14 +1,10 @@
 <div class="content-section">
-    <h3>Our Sidebar</h3>
-    <p class='text-muted'>You can put any information here you'd like.
+    <h3>Let's Meet Awesome People</h3>
+    <p class='text-muted'>
       <ul class="list-group">
           @foreach ($user_info as $item)
           <li class="list-group-item list-group-item-light">{{ $item->name }}</li>
           @endforeach
       </ul>
     </p>
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit" class="p-lg-5">Search</button>
-    </form>
   </div>
