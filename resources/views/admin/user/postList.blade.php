dc14255908b547342cd1249b5fbc477cac3c8c64
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-01-25 02:09:13 +0600

    !

--- resources/views/admin/user/postList.blade.php
+++ resources/views/admin/user/postList.blade.php
@@ -3,10 +3,11 @@
 <div class="my-3 p-3 bg-body rounded shadow-sm">
     @foreach ($posts as $item)
     <div class="d-flex text-muted pt-3">
-      <div class="col-1">
-        {{-- @dd($user->profile->profileImage()) --}}
-        <img src="/storage/{{ $item->image }}" class="img-responsive img-rounded" width="60" height="90" object-fit ="cover">
-      </div>
+        @if ($item->image == Null)
+        <div class="col-1">
+            <img src="/storage/{{ $item->image }}" class="img-responsive img-rounded" width="60" height="90" object-fit ="cover">
+        </div>
+        @endif
       <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
         <div class="d-flex justify-content-between">
           <strong class="text-gray-dark"><span>Post Caption: </span> {{$item->caption}} </strong>
