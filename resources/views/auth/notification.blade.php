2f3bc94ead0d1532238864f8e170ed756771b4c3
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-01-26 20:13:16 +0600

    showing notification list

--- resources/views/auth/notification.blade.php
+++ resources/views/auth/notification.blade.php
@@ -1,19 +1,21 @@
 @extends('layout.base')
 @section('content')
 <div class="list-group">
-    <div class="list-group-item list-group-item-action list-group-item-primary active">Follower List</div>
-    {{-- @foreach ($user as $follower) --}}
-    <a href=" # " class="list-group-item list-group-item-action list-group-item-primary disabled">
-        <div class="row">
-            <div class="row-1 m-0 aligen-center">
-                <img src="#" width="30" height="30" class="rounded-circle d-inline-block align-top" alt="">
-                <strong>#</strong>
+    <div class="list-group-item list-group-item-action list-group-item-primary active align-content-center">Notifications</div>
+    @foreach ($notice as $item)
+    <a href=" {{ route('profile.show',$item->data["follower_details"]["id"]) }} " class="list-group-item list-group-item-action list-group-item-primary disabled">
+        <div class="row p-1">
+            <div class="row-1 aligen-center">
+                <strong>{{ $item->data["follower_details"]["name"] }}</strong>
+            </div>
+            <div class="col-7">
+                {{ $item->data["message"] }}
             </div>
             <div class="col-2">
-                👥
+                <i class="fas fa-user-check"></i>
             </div>
         </div>
     </a>
-    {{-- @endforeach --}}
+    @endforeach
 </div>
 @endsection
