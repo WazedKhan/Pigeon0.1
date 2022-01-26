245bbc060abfdeaf7af9b0c63fbc812c72cdb1af
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-01-26 16:01:08 +0600

    added to notifiy if follow

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Notifications\NewFollower;
 
 class FollowController extends Controller
 {
-    
+
     public function __construct()
     {
         $this->middleware('auth');
@@ -17,8 +18,8 @@ class FollowController extends Controller
     public function addFollow($user)
     {
         $following_id = User::find($user);
-
         $following_id->following()->attach(Auth::user()->id);
+        $following_id->notify(new NewFollower());
         return redirect()->back();
     }
 
