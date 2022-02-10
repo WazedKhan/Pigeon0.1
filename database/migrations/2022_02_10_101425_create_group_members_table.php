a856e6324a1ec37c15e26ae63eaed5abce278fd7
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-02-10 18:23:08 +0600

    group member table

---
+++ database/migrations/2022_02_10_101425_create_group_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_members', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('group_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title')->default('member');
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_members');
+    }
+}
