8e4a1a5600434bdbdd41079b30466c441c0dd9fe
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-02-15 13:46:50 +0600

    share model

---
+++ app/Models/Share.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Share extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
